1e1d56a17729a1de8457bca093f6dcccadf746cd
Ramprakash <prakashram1996@gmail.com>
2021-04-10 01:37:00 -0400

    Package - 2

--- Package/build/lib/restaurant_analysis/sentiment_prediction.py
+++ Package/build/lib/restaurant_analysis/sentiment_prediction.py
@@ -1,5 +1,17 @@
 import pkgutil
 import pickle
 
-model_pipeline = pkgutil.get_data('restaurant_analysis', 'bin/model_pipeline.pkl')
-review_prediction = pickle.loads(model_pipeline)
+
+class SentimentPredictor:
+    def __init__(self):
+        model_pipeline = pkgutil.get_data('restaurant_analysis', 'bin/model_pipeline.pkl')
+        self.review_prediction = pickle.loads(model_pipeline)
+        self.prediction = []
+        self.acceptance_rate = 0
+
+    def predict(self, x):
+        self.prediction = self.review_prediction.predict(x)
+        return self.prediction
+
+    def acceptance(self):
+        self.acceptance_rate = sum(self.prediction)/len(self.prediction)
